e80725f2a4ee4fd9fb693db09da21d2e1c580ac8
ronthalanki <sthalanki@berkeley.edu>
2019-09-29 10:36:01 -0700

    Add compareTo method for Agent

--- src/main/java/blackjack/agents/Agent.java
+++ src/main/java/blackjack/agents/Agent.java
@@ -3,10 +3,11 @@ package blackjack.agents;
 import blackjack.Util;
 import blackjack.models.Card;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lombok.Getter;
 
-public abstract class Agent {
+public abstract class Agent implements Comparable<Agent> {
   @Getter private final List<Card> hand;
   private final boolean isPlaying;
 
@@ -28,5 +29,21 @@ public abstract class Agent {
       return true;
     }
     return false;
+  }
+
+  @Override
+  public int compareTo(final Agent other) {
+    final List<Integer> thisScore = this.getScores();
+    final List<Integer> thatScore = other.getScores();
+
+    if (thisScore.size() == 0 && thatScore.size() == 0) {
+      return 0;
+    } else if (thisScore.size() == 0) {
+      return -1;
+    } else if (thatScore.size() == 0) {
+      return 1;
+    } else {
+      return Collections.max(thisScore).compareTo(Collections.max(thatScore));
+    }
   }
 }
