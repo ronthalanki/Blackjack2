10c545bc6693d42fb3406517f6198227b4913b81
Sairanjith Thalanki <sthalanki@Sairanjiths-MacBook-Pro.local>
2019-09-28 22:56:12 -0700

    Add main method

--- src/main/java/blackjack/Blackjack.java
+++ src/main/java/blackjack/Blackjack.java
@@ -1,3 +1,11 @@
 package blackjack;
 
-public class Blackjack {}
+import blackjack.models.Deck;
+import java.util.Scanner;
+
+public class Blackjack {
+  public static final Deck deck = new Deck();
+  public static final Scanner scan = new Scanner(System.in);
+
+  public static void main(String[] args) {}
+}
