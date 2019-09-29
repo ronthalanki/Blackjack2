3398ae3883febcedec82625b262244c5f898e9ca
Sairanjith Thalanki <sthalanki@Sairanjiths-MacBook-Pro.local>
2019-09-29 08:12:08 -0700

    Add ToString annotation for Card class

--- src/main/java/blackjack/models/Card.java
+++ src/main/java/blackjack/models/Card.java
@@ -2,8 +2,10 @@ package blackjack.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
+@ToString
 public class Card {
   @Getter private final Suit suit;
   @Getter private final Rank rank;
