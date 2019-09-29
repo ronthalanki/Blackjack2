a077dc901534850497894e401fea76101e85d7cd
ronthalanki <sthalanki@berkeley.edu>
2019-09-29 09:01:16 -0700

    Add Random class parameter

--- src/main/java/blackjack/models/Deck.java
+++ src/main/java/blackjack/models/Deck.java
@@ -3,20 +3,27 @@ package blackjack.models;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Deck {
   private final List<Card> deck;
+  private final Random random;
 
   public Deck() {
-    deck = new ArrayList<>();
-    resetDeck();
+    this(new Random());
+  }
+
+  public Deck(final Random random) {
+    this.deck = new ArrayList<>();
+    this.random = random;
+    resetDeck(random);
   }
 
   public Card drawCard() {
     return deck.remove(deck.size() - 1);
   }
 
-  private void resetDeck() {
+  private void resetDeck(final Random random) {
     deck.clear();
 
     for (final Suit s : Suit.values()) {
@@ -25,6 +32,6 @@ public class Deck {
       }
     }
 
-    Collections.shuffle(deck);
+    Collections.shuffle(deck, random);
   }
 }
