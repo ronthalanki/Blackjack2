d8b72d6788b208fc102facf1aa65bda9b051d776
ronthalanki <sthalanki@berkeley.edu>
2019-09-29 19:23:42 -0700

    Add getScore functionality to Agent class

--- src/main/java/blackjack/agents/Agent.java
+++ src/main/java/blackjack/agents/Agent.java
@@ -1,25 +1,48 @@
 package blackjack.agents;
 
-import blackjack.Util;
 import blackjack.models.Card;
+import blackjack.models.Rank;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 
 public abstract class Agent implements Comparable<Agent> {
   @Getter private final List<Card> hand;
+  @Getter private List<Integer> scores;
 
   public Agent() {
     this.hand = new ArrayList<Card>();
+    this.scores = new ArrayList<Integer>();
+
+    this.scores.add(0);
   }
 
   public void addCard(final Card c) {
     hand.add(c);
-  }
 
-  public List<Integer> getScores() {
-    return Util.getScore(hand);
+    if (c.getRank() == Rank.ACE) {
+      final List<Integer> scoresCopy = new ArrayList<Integer>(scores);
+      for (int i = 0; i < scoresCopy.size(); i++) {
+        scores.add(scoresCopy.get(i));
+      }
+
+      for (int i = 0; i < scores.size() / 2; i++) {
+        scores.set(i, scores.get(i) + 1);
+      }
+
+      for (int i = scores.size() / 2; i < scores.size(); i++) {
+        scores.set(i, scores.get(i) + 11);
+      }
+    } else {
+      for (int i = 0; i < scores.size(); i++) {
+        scores.set(i, scores.get(i) + c.getRank().value);
+      }
+    }
+
+    scores =
+        scores.stream().mapToInt(i -> i).filter(i -> i <= 21).boxed().collect(Collectors.toList());
   }
 
   public boolean isPlaying() {
