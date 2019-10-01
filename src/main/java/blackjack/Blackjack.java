c8e75ad98fd4b741ce51bd161b4f228339fe8a3d
ronthalanki <sthalanki@berkeley.edu>
2019-09-30 18:10:54 -0700

    Add Blackjack class

--- src/main/java/blackjack/Blackjack.java
+++ src/main/java/blackjack/Blackjack.java
@@ -1,11 +1,91 @@
 package blackjack;
 
+import blackjack.agents.Agent;
+import blackjack.agents.BotAgent;
+import blackjack.agents.PlayerAgent;
 import blackjack.models.Deck;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
 
+@Builder
 public class Blackjack {
-  public static final Deck deck = new Deck();
-  public static final Scanner scan = new Scanner(System.in);
+  @Getter @NonNull @Builder.Default private final Deck deck = new Deck();
+  @Getter @NonNull @Builder.Default private final BotAgent dealer = new BotAgent();
+  @Getter @NonNull @Builder.Default private final PlayerAgent player = new PlayerAgent();
+  private static final String template = "%s State: %s, Scores: %s\n";
 
-  public static void main(String[] args) {}
+  @NonNull @Builder.Default
+  private final List<BotAgent> bots = Arrays.asList(new BotAgent(), new BotAgent());
+
+  public List<Agent> getAllAgents() {
+    final ArrayList<Agent> agents = new ArrayList<Agent>();
+    agents.add(player);
+    agents.addAll(bots);
+    agents.add(dealer);
+
+    return agents;
+  }
+
+  public List<BotAgent> getAllBots() {
+    final ArrayList<BotAgent> allBots = new ArrayList<BotAgent>();
+    allBots.addAll(bots);
+    allBots.add(dealer);
+
+    return allBots;
+  }
+
+  public String getState() {
+    String gameState = "";
+
+    gameState += String.format(template, "Dealer", dealer.getHand(), dealer.getScores());
+    gameState += String.format(template, "Player", player.getHand(), player.getScores());
+
+    for (final BotAgent botAgent : bots) {
+      gameState += String.format(template, "Bot", botAgent.getHand(), botAgent.getScores());
+    }
+
+    return gameState;
+  }
+
+  public static String getAgentState(final Agent agent) {
+    return String.format(template, agent, agent.getHand(), agent.getScores());
+  }
+
+  public static void main(String[] args) {
+    final Blackjack game = Blackjack.builder().deck(new Deck(new Random(2))).build();
+    final Scanner scan = new Scanner(System.in);
+
+    for (final Agent agent : game.getAllAgents()) {
+      for (int i = 0; i < 2; i++) {
+        agent.addCard(game.getDeck().drawCard());
+      }
+    }
+
+    System.out.println(game.getState());
+
+    while (game.getPlayer().getNextCard(scan)) {
+      game.getPlayer().addCard(game.getDeck().drawCard());
+      System.out.println(game.getAgentState(game.getPlayer()));
+    }
+
+    for (final BotAgent bot : game.getAllBots()) {
+      while (bot.getNextCard()) {
+        bot.addCard(game.getDeck().drawCard());
+      }
+    }
+
+    System.out.println(game.getState());
+
+    if (game.getDealer().compareTo(game.getPlayer()) == 1) {
+      System.out.println("Loser");
+    } else {
+      System.out.println("Winner");
+    }
+  }
 }
